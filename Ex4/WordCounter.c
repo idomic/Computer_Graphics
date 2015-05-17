88e17662d74dd104dfc6ad489d35d4650724d6e1
danaterlich <danaterlich@hotmail.com>
2015-05-17 19:00:37 +0300

    count words in word counter not finished

--- Ex4/WordCounter.c
+++ Ex4/WordCounter.c
@@ -1,3 +1,5 @@
+// Dana Erlich 200400950
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -54,8 +56,10 @@ int is_alphabetic (unsigned char c) {
  void *run_listener(void *param) {
 	ListenerData *data;
 	BoundedBuffer *buff;
-	int fd, num;
+	int fd, num, len;
 	char message[READ_BUFF_SIZE];
+	char *token;
+	char *token_filename;
 
 	data = (ListenerData*)param;
 	buff = data->buff;
@@ -66,17 +70,108 @@ int is_alphabetic (unsigned char c) {
 		// Wait for a connection on the pipe
 		fd = open(data->pipe_file, O_RDONLY);
 		if (fd <= 0) {
-			// Handle error
+			perror("open");
+			exit(1);		
 		}
 
 		// Read data from pipe
 		while ((num = read(fd, message, READ_BUFF_SIZE)) > 0) {
-			// Parse read data into messages, enqueue them, etc...
-			// TODO: Complete this part
-			// TODO: Make sure the thread exits at some point
+			message[num-1] = '\0';
+			// Parse seperate file names by new line
+			token = strtok(message,"\n");
+			
+			while (token != NULL) {
+				len = strlen(token);
+				// Allocate memory for a file name
+				token_filename = (char*) malloc (len + 1);
+				if (token_filename == NULL) {
+					fprintf(stderr, "Error: Cannot allocate memory.\n");
+					exit (1);
+				}
+				// Copy  file name from token to token_filename
+				strncpy(token_filename, token, len);
+				token_filename[len] = '\0';
+				// Enqueue the file name to the files queue
+				enqueue_result = bounded_buffer_enqueue(buff, token_filename);
+				if (enqueue_result == 0) { // Enqueue has failed
+					// Free the last allocated file name buffer
+					free(token_filename);
+					// Remove the pipe file
+					if(remove(data->pipe_file) != 0) {
+						fprintf(stderr, "Error: pipe file could not be removed.\n");
+					}
+					return NULL;
+				}
+				//File name has enqueud successfully.
+				token = strtok(NULL, "\n");
+			}
 		}
 		close(fd);
 	}
 }
 
-// TODO: Implement the other required functions
+/*
+ * WordCounter thread starting point.
+ * The word counter reads file names from the files queue, one by one, and counts the words in them.
+ * It will write the result to a log file.
+ * Then it should free the memory of the dequeued file name string (it was allocated by the Listener thread).
+ * If the dequeue operation fails (returns NULL), it means that the application is trying
+ * to exit and therefore the thread should simply terminate.
+ */
+void *run_wordcounter(void *param){
+
+}
+/*
+ * A word-counting function. Counts the words in file_name and returns the number.
+ */
+int count_words_in_file(char *file_name){
+	unsigned char words_buffer[FILE_READ_BUFFER_SIZE];
+	FILE *file;
+	int len = 0;	
+	int word_counter;
+	if (file_name == NULL) {
+		return 0;
+	}
+
+	file = fopen(file_name, "r");
+	if (file == NULL) {
+		return 0;
+	}
+
+	while ((len = fread(words_buffer, 1, FILE_READ_BUFFER_SIZE, file)) > 0) {
+		int current_char = 0;
+		while((words_buffer[current_char] != EOF) && (current_char != (FILE_READ_BUFFER_SIZE - 1))){
+			if(!is_alphabetic([current_char])) {
+				word_counter++;
+		}
+			current_char++;
+	}
+
+    fclose(file);
+	return word_counter;
+}
+
+/*
+ * logs the number of words in the file to the output log file.
+ */
+void log_count(WordCounterData *counter_data, char *file_name, int count);
+
+
+
+/*
+ * Main function.
+ * Reads command line arguments in the format:
+ * 		./WordCounter pipe_name destination_log_file_name
+ * Where pipe_name is the name of FIFO pipe that the Listener should create and
+ * destination_log_file_name is the destination file name where the log should be written.
+ * This function should create the files queue and prepare the parameters to the Listener and
+ * WordCounter threads. Then, it should create these threads.
+ * After threads are created, this function should control them as follows:
+ * it should read input from user, and if the input line is "exit" (or "exit\n"), it should
+ * set the files queue as "finished". This should make the threads terminate (possibly only
+ * when the next connection is received).
+ * At the end the function should join the threads and exit.
+ */
+int main(int argc, char *argv[]);
+
+
