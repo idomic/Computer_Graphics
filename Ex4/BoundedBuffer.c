d94503e0bbed38fb0d98d9ef64e77a7b384f3364
danaterlich <danaterlich@hotmail.com>
2015-05-17 18:36:54 +0300

    finished boundedbuffer

--- Ex4/BoundedBuffer.c
+++ Ex4/BoundedBuffer.c
@@ -1,9 +1,12 @@
-//200400950
+//Ido Michael 201157138
+//Dana Erlich 200400950
 
 #ifndef BOUNDEDBUFFER_H_
 #define BOUNDEDBUFFER_H_
 
 #include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct {
 	char **buffer;
@@ -68,9 +71,10 @@ int bounded_buffer_enqueue(BoundedBuffer *buff, char *data){
 	buff->empty = FALSE;
 
 	if (buff->finished == 1) {
-		pthread_cond_signal(&(buff->cv_empty)); // Signal for any thread waiting for a value
 		return 0;
 	}
+	
+	pthread_cond_signal(&(buff->cv_empty)); // Signal for any thread waiting for a value
 	
 	pthread_mutex_unlock(&(buff->mutex)); // Exit critical section
 
@@ -86,7 +90,31 @@ int bounded_buffer_enqueue(BoundedBuffer *buff, char *data){
  * This function should be synchronized on the buffer's mutex!
  */
 char *bounded_buffer_dequeue(BoundedBuffer *buff){
+	if (buff->finished == 1) {
+		return 0;
+	}
 
+	pthread_mutex_lock(&(buff->mutex)); // Begin critical section
+	
+	if (buff->size == 0) { // Wait until it is not empty
+		pthread_cond_wait(&(buff->cv_empty), &(buff->mutex));
+	}
+	
+	// dequeue data and set to 'not full'
+	data = buff->buffer[buff->tail];
+	buff->tail = ((buff->tail) - 1) % buff->capacity;
+	buff->size--;
+	buff->full = FALSE;
+
+	if (buff->finished == 1) {
+		return 0;
+	}
+
+	pthread_cond_signal(&(buff->cv_full)); // Signal for any thread waiting for a value
+	
+	pthread_mutex_unlock(&(buff->mutex)); // Exit critical section
+
+	return 1;
 }
 
 /*
@@ -95,11 +123,32 @@ char *bounded_buffer_dequeue(BoundedBuffer *buff){
  * waiting on the condition variables of this buffer.
  * This function should be synchronized on the buffer's mutex!
  */
-void bounded_buffer_finish(BoundedBuffer *buff);
+void bounded_buffer_finish(BoundedBuffer *buff){
+	
+	pthread_mutex_lock(&(buff->mutex)); // Begin critical section
+	
+	buff->finished = 1;
+	
+	pthread_cond_broadcast(&(buff->cv_full));
+	pthread_cond_broadcast(&(buff->cv_empty));
+
+	pthread_mutex_unlock(&(buff->mutex)); // Exit critical section
+}
 
 /*
  * Frees the buffer memory and destroys mutex and condition variables.
  */
-void bounded_buffer_destroy(BoundedBuffer *buff);
+void bounded_buffer_destroy(BoundedBuffer *buff){
+	int i;
+	for (i = 0; i < capacity; i++)
+	{
+		res = buff->buffer[i];
+		free(res);
+	}
+	pthread_mutex_destroy(&(cubbyHole->mutex)); // Destroy mutex
+	pthread_cond_destroy(&(cubbyHole->cv_empty)); // Destroy empty condition
+	pthread_cond_destroy(&(cubbyHole->cv_full));  // Destroy full condition
+	free(buff);
+}
 
 #endif /* BOUNDEDBUFFER_H_ */
