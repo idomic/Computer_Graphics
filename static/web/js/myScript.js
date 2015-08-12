8930cee76ad1e48626ea3cde215ca7ed73645199
danaterlich <danaterlich@hotmail.com>
2015-08-12 21:02:10 +0300

    trying to add image from gallery

--- static/web/js/myScript.js
+++ static/web/js/myScript.js
@@ -30,3 +30,24 @@ function myFunction(str){
     document.getElementById("flb-close").click();
 
     };
+
+
+var myApp = angular.module('myApp',[]);
+
+
+function MyCtrl($scope) {
+    $scope.stepsModel = [];
+
+    $scope.imageUpload = function(element){
+        var reader = new FileReader();
+        reader.onload = $scope.imageIsLoaded;
+        reader.readAsDataURL(element.files[0]);
+    }
+
+    $scope.imageIsLoaded = function(e){
+        $scope.$apply(function() {
+            $scope.stepsModel.push(e.target.result);
+        });
+    }
+}
+    
